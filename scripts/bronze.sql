8ec6cb9aead3831c519abda41278860bde7d74dd
Ahmed Yousry <111189099+Ahmeddyousryy@users.noreply.github.com>
2026-04-24 22:26:21 +0300

    Add bulk insert for CRM and ERP tables
    Added bulk insert statements to load data from CSV files into the bronze schema tables after truncating them.

--- scripts/bronze.sql
+++ scripts/bronze.sql
@@ -1,5 +1,6 @@
 USE DataWarehouse;
 
+/* Creating Tables */
 
 IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL
 	DROP TABLE bronze.crm_cust_info
@@ -66,5 +67,73 @@ CREATE TABLE bronze.erp_px_cat_g1v2 (
 	SUBCAT NVARCHAR(50),
 	MAINTENANCE NVARCHAR(10)
 
+);
+
+
+/* Loading data from csv file into the SQL server tables */
+
+TRUNCATE TABLE bronze.crm_cust_info;
+BULK INSERT bronze.crm_cust_info
+FROM 'D:\Courses\Data Engineering\Data Warehouse\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+WITH(
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK,
+	FORMAT = 'csv'
+);
+
+
+TRUNCATE TABLE bronze.crm_prd_info;
+BULK INSERT bronze.crm_prd_info
+FROM 'D:\Courses\Data Engineering\Data Warehouse\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+WITH(
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK,
+	FORMAT = 'csv'
+);
+
+
+TRUNCATE TABLE bronze.crm_sales_details;
+BULK INSERT bronze.crm_sales_details
+FROM 'D:\Courses\Data Engineering\Data Warehouse\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+WITH(
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK,
+	FORMAT = 'csv'
+);
+
+
+TRUNCATE TABLE bronze.erp_cust_az12;
+BULK INSERT bronze.erp_cust_az12
+FROM 'D:\Courses\Data Engineering\Data Warehouse\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
+WITH(
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK,
+	FORMAT = 'csv'
+);
+
+
+TRUNCATE TABLE bronze.erp_loc_a101;
+BULK INSERT bronze.erp_loc_a101
+FROM 'D:\Courses\Data Engineering\Data Warehouse\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
+WITH(
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK,
+	FORMAT = 'csv'
+);
+
+
+TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+BULK INSERT bronze.erp_px_cat_g1v2
+FROM 'D:\Courses\Data Engineering\Data Warehouse\sql-data-warehouse-project\datasets\source_erp\px_cat_g1v2.csv'
+WITH(
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK,
+	FORMAT = 'csv'
 );
 
