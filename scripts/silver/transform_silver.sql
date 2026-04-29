c765db790fbdeea5d687cc03943e5d75814738c5
Ahmed Yousry Mohamed <111189099+Ahmeddyousryy@users.noreply.github.com>
2026-04-30 01:36:37 +0300

    Refactor transform_silver procedure for ETL process
    This stored procedure performs an ETL process to populate the 'silver' schema tables from the 'bronze' schema tables, including cleaning and transforming data.

--- scripts/silver/transform_silver.sql
+++ scripts/silver/transform_silver.sql
@@ -1,178 +1,287 @@
+/*
+=============================================================================
+Stored Procedure: Clean, Transform and Load Data (bronze -> silver)
+=============================================================================
 
-USE DataWarehouse
+Script Purpose:
+	This stored procedure performs ETL (Extract, Transform, Load) process to
+	populate the 'silver' schema tables from the 'bronze' schema tables.
+
+	It performs the following actions:
+	- Truncates the silver tables before loading 
+	- Clean, Transform and then Load into silver schema tables
+
+Parameters:
+	None.
+
+Usage Example:
+	EXEC silver.transform_silver;
+
+*/
+
+
+
+CREATE OR ALTER PROCEDURE silver.transform_silver AS
+BEGIN
+	
+	DECLARE @transform_start_time DATETIME , @transform_end_time DATETIME
+
+	DECLARE @start_time DATETIME , @end_time DATETIME
 
 ------------------------------- CLEANING AND TRANSFORMING TABLE bronze.crm_cust_info --------------------------
-TRUNCATE TABLE silver.crm_cust_info;
-INSERT INTO silver.crm_cust_info(
-	cst_id,
-	cst_key,
-	cst_firstname,
-	cst_lastname,
-	cst_material_status,
-	cst_gndr,
-	cst_create_date
-)
-SELECT
-	cst_id,
-	cst_key,
-	TRIM(cst_firstname) AS cst_firstname,
-	TRIM(cst_lastname) AS cst_lastname,
+	
+	BEGIN TRY
 
-	CASE
-		WHEN UPPER(TRIM(cst_material_status)) = 'S' THEN 'Single'
-		WHEN UPPER(TRIM(cst_material_status)) = 'M' THEN 'Married'
-		ELSE 'n/a'
-	END cst_material_status,
+		SET @start_time = GETDATE();
 
-	CASE
-		WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
-		WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
-		ELSE 'n/a'
-	END cst_gndr,
-	cst_create_date
+	    PRINT '=====================================================================';
+		PRINT 'Transforming Silver Layer';
+		PRINT '=====================================================================';
+		PRINT '';
+		
+	
 
-FROM (
+		SET @transform_start_time = GETDATE();
+		PRINT '>> Truncating Table : silver.crm_cust_info';
+		TRUNCATE TABLE silver.crm_cust_info;
+
+		PRINT '>> Transforming and Loading Cleaned Data Into : silver.crm_cust_info'
+		INSERT INTO silver.crm_cust_info(
+			cst_id,
+			cst_key,
+			cst_firstname,
+			cst_lastname,
+			cst_material_status,
+			cst_gndr,
+			cst_create_date
+		)
 		SELECT
-		*,
-		ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) AS flag_last
-		FROM bronze.crm_cust_info
-		WHERE cst_id IS NOT NULL
-)t
-WHERE flag_last = 1 
+			cst_id,
+			cst_key,
+			TRIM(cst_firstname) AS cst_firstname,
+			TRIM(cst_lastname) AS cst_lastname,
+
+			CASE
+				WHEN UPPER(TRIM(cst_material_status)) = 'S' THEN 'Single'
+				WHEN UPPER(TRIM(cst_material_status)) = 'M' THEN 'Married'
+				ELSE 'n/a'
+			END cst_material_status,
+
+			CASE
+				WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
+				WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
+				ELSE 'n/a'
+			END cst_gndr,
+			cst_create_date
+
+		FROM (
+				SELECT
+				*,
+				ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) AS flag_last
+				FROM bronze.crm_cust_info
+				WHERE cst_id IS NOT NULL
+		)t
+		WHERE flag_last = 1 
+
+		SET @transform_end_time = GETDATE();
+		PRINT '>> Transform Duration: ' + CAST(DATEDIFF(second,@transform_start_time,@transform_end_time) AS NVARCHAR) + ' seconds';
+		PRINT '---------------------------';
+		PRINT '';
 
 
+		------------------------------- CLEANING AND TRANSFORMING TABLE bronze.crm_prd_info --------------------------
+
+		SET @transform_start_time = GETDATE();
+		PRINT '>> Truncating Table : silver.crm_prd_info';
+		TRUNCATE TABLE silver.crm_prd_info;
+
+		PRINT '>> Transforming and Loading Cleaned Data Into : silver.crm_prd_info'
+		INSERT INTO silver.crm_prd_info (
+		prd_id,
+		cat_id,
+		prd_key,
+		prd_nm,
+		prd_cost,
+		prd_line,
+		prd_start_dt,
+		prd_end_dt
+		)
+		SELECT
+			prd_id,
+			REPLACE(SUBSTRING(prd_key,1,5) ,'-','_') AS cat_id,
+			SUBSTRING(prd_key,7,LEN(prd_key)) AS prd_key,
+			prd_nm,
+			ISNULL(prd_cost,0) AS prd_cost,
+			CASE UPPER(TRIM(prd_line))
+				WHEN 'M' THEN 'Mountain'
+				WHEN 'R' THEN 'Road'
+				WHEN 'S' THEN 'Other Sales'
+				WHEN 'T' THEN 'Touring'
+				ELSE 'n/a'
+			END AS prd_line,
+			CAST(prd_start_dt AS DATE),
+			CAST(DATEADD(DAY,-1,LEAD(prd_start_dt,1) OVER(PARTITION BY prd_key ORDER BY prd_start_dt)) AS DATE) AS prd_end_dt
+		FROM bronze.crm_prd_info;
+
+		SET @transform_end_time = GETDATE();
+		PRINT '>> Transform Duration: ' + CAST(DATEDIFF(second,@transform_start_time,@transform_end_time) AS NVARCHAR) + ' seconds';
+		PRINT '---------------------------';
+		PRINT '';
 
 
-------------------------------- CLEANING AND TRANSFORMING TABLE bronze.crm_prd_info --------------------------
-TRUNCATE TABLE silver.crm_prd_info;
-INSERT INTO silver.crm_prd_info (
-prd_id,
-cat_id,
-prd_key,
-prd_nm,
-prd_cost,
-prd_line,
-prd_start_dt,
-prd_end_dt
-)
-SELECT
-	prd_id,
-    REPLACE(SUBSTRING(prd_key,1,5) ,'-','_') AS cat_id,
-	SUBSTRING(prd_key,7,LEN(prd_key)) AS prd_key,
-	prd_nm,
-	ISNULL(prd_cost,0) AS prd_cost,
-	CASE UPPER(TRIM(prd_line))
-		WHEN 'M' THEN 'Mountain'
-		WHEN 'R' THEN 'Road'
-		WHEN 'S' THEN 'Other Sales'
-		WHEN 'T' THEN 'Touring'
-		ELSE 'n/a'
-	END AS prd_line,
-	CAST(prd_start_dt AS DATE),
-	CAST(DATEADD(DAY,-1,LEAD(prd_start_dt,1) OVER(PARTITION BY prd_key ORDER BY prd_start_dt)) AS DATE) AS prd_end_dt
-FROM bronze.crm_prd_info;
+		------------------------------- CLEANING AND TRANSFORMING TABLE bronze.crm_sales_details --------------------------
+
+		SET @transform_start_time = GETDATE();
+		PRINT '>> Truncating Table : silver.crm_sales_details';
+		TRUNCATE TABLE silver.crm_sales_details;
+
+		PRINT '>> Transforming and Loading Cleaned Data Into : silver.crm_sales_details'
+		INSERT INTO silver.crm_sales_details(
+			sls_ord_num,
+			sls_prd_key,
+			sls_cust_id,
+			sls_order_dt,
+			sls_ship_dt,
+			sls_due_dt,
+			sls_sales,
+			sls_quantity,
+			sls_price
+		)
+		SELECT
+		sls_ord_num,
+		sls_prd_key,
+		sls_cust_id,
+		CASE 
+			WHEN LEN(sls_order_dt) != 8 OR sls_order_dt < 0 THEN NULL
+			ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE)
+		END AS sls_order_dt,
+
+		CASE 
+			WHEN LEN(sls_ship_dt) != 8 OR sls_ship_dt < 0 THEN NULL
+			ELSE CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE)
+		END AS sls_ship_dt,
+
+		CASE 
+			WHEN LEN(sls_due_dt) != 8 OR sls_due_dt < 0 THEN NULL
+			ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE)
+		END AS sls_due_dt,
+
+		CASE 
+			WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity *  ABS(sls_price) THEN sls_quantity *  ABS(sls_price)
+			ELSE  sls_sales
+		END AS sls_sales,
+
+		sls_quantity,
+
+		CASE
+			WHEN sls_price IS NULL OR sls_price <= 0 THEN sls_sales / NULLIF(sls_quantity,0)
+			ELSE  sls_price
+		END AS sls_price
+		FROM bronze.crm_sales_details
+
+		SET @transform_end_time = GETDATE();
+		PRINT '>> Transform Duration: ' + CAST(DATEDIFF(second,@transform_start_time,@transform_end_time) AS NVARCHAR) + ' seconds';
+		PRINT '---------------------------';
+		PRINT '';
 
 
+		------------------------------- CLEANING AND TRANSFORMING TABLE bronze.erp_cust_az12 --------------------------
 
-------------------------------- CLEANING AND TRANSFORMING TABLE bronze.crm_sales_details --------------------------
+		SET @transform_start_time = GETDATE();
+		PRINT '>> Truncating Table : silver.erp_cust_az12';
+		TRUNCATE TABLE silver.erp_cust_az12;
 
-TRUNCATE TABLE silver.crm_sales_details;
-INSERT INTO silver.crm_sales_details(
-	sls_ord_num,
-	sls_prd_key,
-	sls_cust_id,
-	sls_order_dt,
-	sls_ship_dt,
-	sls_due_dt,
-	sls_sales,
-	sls_quantity,
-	sls_price
-)
-SELECT
-sls_ord_num,
-sls_prd_key,
-sls_cust_id,
-CASE 
-	WHEN LEN(sls_order_dt) != 8 OR sls_order_dt < 0 THEN NULL
-	ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE)
-END AS sls_order_dt,
+		PRINT '>> Transforming and Loading Cleaned Data Into : silver.erp_az12'
+		INSERT INTO silver.erp_cust_az12(
+			CID,
+			BDATE,
+			GEN
+		)
+		SELECT 
+			CASE
+				WHEN CID LIKE 'NAS%' THEN SUBSTRING(CID,4,LEN(CID))
+				ELSE CID
+			END AS CID,
+			CASE
+				WHEN BDATE > GETDATE() OR BDATE < '1900-01-01' THEN NULL
+				ELSE BDATE
+			END BDATE,
+			CASE
+				WHEN LOWER(TRIM(GEN)) IN ('male','m')   THEN 'Male'
+				WHEN LOWER(TRIM(GEN)) IN ('female','f') THEN 'Female'
+				ELSE 'n/a'
+			END AS GEN
+		FROM bronze.erp_cust_az12
 
-CASE 
-	WHEN LEN(sls_ship_dt) != 8 OR sls_ship_dt < 0 THEN NULL
-	ELSE CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE)
-END AS sls_ship_dt,
+		SET @transform_end_time = GETDATE();
+		PRINT '>> Transform Duration: ' + CAST(DATEDIFF(second,@transform_start_time,@transform_end_time) AS NVARCHAR) + ' seconds';
+		PRINT '---------------------------';
+		PRINT '';
 
-CASE 
-	WHEN LEN(sls_due_dt) != 8 OR sls_due_dt < 0 THEN NULL
-	ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE)
-END AS sls_due_dt,
+		------------------------------- CLEANING AND TRANSFORMING TABLE bronze.erp_loc_a101 --------------------------
 
-CASE 
-	WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity *  ABS(sls_price) THEN sls_quantity *  ABS(sls_price)
-	ELSE  sls_sales
-END AS sls_sales,
+		SET @transform_start_time = GETDATE();
+		PRINT '>> Truncating Table : silver.erp_loc_a101';
+		TRUNCATE TABLE silver.erp_loc_a101;
 
-sls_quantity,
+		PRINT '>> Transforming and Loading Cleaned Data Into : silver.erp_loc_a101'
+		INSERT INTO silver.erp_loc_a101(
+			CID,
+			CNTRY
+		)
+		SELECT
+			REPLACE(CID,'-','') CID,
+			CASE
+				WHEN TRIM(CNTRY) = 'DE' THEN 'Germany'
+				WHEN TRIM(CNTRY) IN ('USA','US')  THEN 'United States'
+				WHEN TRIM(CNTRY) = '' OR CNTRY IS NULL THEN 'n/a'
+				ELSE CNTRY
+			END AS CNTRY
+		FROM bronze.erp_loc_a101
 
-CASE
-	WHEN sls_price IS NULL OR sls_price <= 0 THEN sls_sales / NULLIF(sls_quantity,0)
-	ELSE  sls_price
-END AS sls_price
-FROM bronze.crm_sales_details
+		SET @transform_end_time = GETDATE();
+		PRINT '>> Transform Duration: ' + CAST(DATEDIFF(second,@transform_start_time,@transform_end_time) AS NVARCHAR) + ' seconds';
+		PRINT '---------------------------';
+		PRINT '';
 
+		------------------------------- CLEANING AND TRANSFORMING TABLE bronze.erp_px_cat_g1v2 --------------------------
 
-------------------------------- CLEANING AND TRANSFORMING TABLE bronze.erp_cust_az12 --------------------------
-TRUNCATE TABLE silver.erp_cust_az12;
-INSERT INTO silver.erp_cust_az12(
-	CID,
-	BDATE,
-	GEN
-)
-SELECT 
-	CASE
-		WHEN CID LIKE 'NAS%' THEN SUBSTRING(CID,4,LEN(CID))
-		ELSE CID
-	END AS CID,
-	CASE
-		WHEN BDATE > GETDATE() OR BDATE < '1900-01-01' THEN NULL
-		ELSE BDATE
-	END BDATE,
-	CASE
-		WHEN LOWER(TRIM(GEN)) IN ('male','m')   THEN 'Male'
-		WHEN LOWER(TRIM(GEN)) IN ('female','f') THEN 'Female'
-		ELSE 'n/a'
-	END AS GEN
-FROM bronze.erp_cust_az12
+		SET @transform_start_time = GETDATE();
+		PRINT '>> Truncating Table : silver.erp_cat_g1v2';
+		TRUNCATE TABLE silver.erp_px_cat_g1v2;
 
-------------------------------- CLEANING AND TRANSFORMING TABLE bronze.erp_loc_a101 --------------------------
-TRUNCATE TABLE silver.erp_loc_a101;
-INSERT INTO silver.erp_loc_a101(
-	CID,
-	CNTRY
-)
-SELECT
-	REPLACE(CID,'-','') CID,
-	CASE
-		WHEN TRIM(CNTRY) = 'DE' THEN 'Germany'
-		WHEN TRIM(CNTRY) IN ('USA','US')  THEN 'United States'
-		WHEN TRIM(CNTRY) = '' OR CNTRY IS NULL THEN 'n/a'
-		ELSE CNTRY
-	END AS CNTRY
-FROM bronze.erp_loc_a101
+		PRINT '>> Transforming and Loading Cleaned Data Into : silver.erp_cat_g1v2'
+		INSERT INTO silver.erp_px_cat_g1v2(
+		ID,
+		CAT,
+		SUBCAT,
+		MAINTENANCE
+		)
+		SELECT
+			ID,
+			CAT,
+			SUBCAT,
+			MAINTENANCE
+		FROM bronze.erp_px_cat_g1v2
 
-------------------------------- CLEANING AND TRANSFORMING TABLE bronze.erp_px_cat_g1v2 --------------------------
+		SET @transform_end_time = GETDATE();
+		PRINT '>> Transform Duration: ' + CAST(DATEDIFF(second,@transform_start_time,@transform_end_time) AS NVARCHAR) + ' seconds';
+		PRINT '---------------------------';
+		PRINT '';
 
-TRUNCATE TABLE silver.erp_px_cat_g1v2;
-INSERT INTO silver.erp_px_cat_g1v2(
-ID,
-CAT,
-SUBCAT,
-MAINTENANCE
-)
-SELECT
-	ID,
-	CAT,
-	SUBCAT,
-	MAINTENANCE
-FROM bronze.erp_px_cat_g1v2
+		SET @end_time = GETDATE();
+		PRINT '===================================================';
+		PRINT 'Transforming Silver Layer is Completed';
+		PRINT '     - Total Transform Duration: ' + CAST (DATEDIFF(second,@start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '===================================================';
+
+	END TRY
+
+	BEGIN CATCH
+		PRINT '==============================================';
+		PRINT 'ERROR OCCURED DURING TRANSFORMING SILVER LAYER'
+		PRINT 'Error Message: ' + ERROR_MESSAGE();
+		PRINT '==============================================';
+	END CATCH
+END
 
