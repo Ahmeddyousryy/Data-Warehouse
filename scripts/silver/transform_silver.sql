b2a48f4e7441776cf7f7ad51aa2198758a475361
Ahmed Yousry Mohamed <111189099+Ahmeddyousryy@users.noreply.github.com>
2026-04-29 21:16:55 +0300

    Implement data transformation for erp_px_cat_g1v2
    Added cleaning and transforming logic for bronze.erp_px_cat_g1v2.

--- scripts/silver/transform_silver.sql
+++ scripts/silver/transform_silver.sql
@@ -159,3 +159,20 @@ SELECT
 		ELSE CNTRY
 	END AS CNTRY
 FROM bronze.erp_loc_a101
+
+------------------------------- CLEANING AND TRANSFORMING TABLE bronze.erp_px_cat_g1v2 --------------------------
+
+TRUNCATE TABLE silver.erp_px_cat_g1v2;
+INSERT INTO silver.erp_px_cat_g1v2(
+ID,
+CAT,
+SUBCAT,
+MAINTENANCE
+)
+SELECT
+	ID,
+	CAT,
+	SUBCAT,
+	MAINTENANCE
+FROM bronze.erp_px_cat_g1v2
+
